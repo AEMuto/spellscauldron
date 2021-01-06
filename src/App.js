cdc929b5b1600d1db2ac5b463c3a54bbb214069d
Antoine <antoine.marseaud@gmail.com>
2021-01-06 18:06:13 +0100

    Install react-table and first test to display data to spellsTable component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import SpellsTable from './components/spellsTable';
 
 class App extends Component {
   state = {};
@@ -21,19 +22,7 @@ class App extends Component {
           <Grid minH="100vh" p={3}>
             <ColorModeSwitcher justifySelf="flex-end" />
             <VStack spacing={8}>
-              <Logo h="40vmin" pointerEvents="none" />
-              <Text>
-                Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-              </Text>
-              <Link
-                color="teal.500"
-                href="https://chakra-ui.com"
-                fontSize="2xl"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn Chakra
-              </Link>
+              <SpellsTable />
             </VStack>
           </Grid>
         </Box>
---
+++ src/components/spellsTable.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react';
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { useTable, useSortBy } from 'react-table';
+import spells from '../data/spells.json';
+
+function SpellsTable() {
+  const data = React.useMemo(() => spells, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Level',
+        accessor: 'level',
+      },
+      {
+        Header: 'Time',
+        accessor: 'casting_time',
+      },
+      {
+        Header: 'School',
+        accessor: 'school',
+      },
+      {
+        Header: 'C.',
+        accessor: 'concentration',
+        isBoolean: true,
+      },
+      {
+        Header: 'R.',
+        accessor: 'ritual',
+        isBoolean: true,
+      },
+      {
+        Header: 'Range',
+        accessor: 'range',
+      },
+      {
+        Header: 'Source',
+        accessor: 'page',
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data }, useSortBy);
+
+  return (
+    <Table {...getTableProps()}>
+      <Thead>
+        {headerGroups.map(headerGroup => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <Th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                isBoolean={column.isBoolean}
+              >
+                {column.render('Header')}
+                <chakra.span pl="4">
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon aria-label="sorted descending" />
+                    ) : (
+                      <TriangleUpIcon aria-label="sorted ascending" />
+                    )
+                  ) : null}
+                </chakra.span>
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map(cell => (
+                <Td {...cell.getCellProps()} isBoolean={cell.column.isBoolean}>
+                  {cell.render('Cell')}
+                </Td>
+              ))}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+}
+
+export default SpellsTable;
